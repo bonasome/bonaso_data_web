3cd190bc29d00e6ecedd29c4b45804ca70c5ee8f
Jay Rosato <rosato.jay@gmail.com>
2025-06-19 21:42:00 +0200

    again

--- src/components/indicators/CreateIndicator.jsx
+++ src/components/indicators/CreateIndicator.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import { useNavigate } from 'react-router-dom';
 import Loading from '../reuseables/Loading';
 import fetchWithAuth from "../../../services/fetchWithAuth";
