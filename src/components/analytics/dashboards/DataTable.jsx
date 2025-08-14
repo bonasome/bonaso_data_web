af3ebf986494468c7d69666f6a84bb70503c257c
Jay <rosato.jay@gmail.com>
2025-08-14 09:26:17 +0200

    Fixed minor big in data tables.

--- src/components/analytics/dashboards/DataTable.jsx
+++ src/components/analytics/dashboards/DataTable.jsx
@@ -6,21 +6,23 @@ export default function DataTable({ data, breakdown1, breakdown2, map }) {
     //breakdown 1 -the name of the legend (alternatively indicator/target)
     //breakdown 2 - the name of the stack
     //map - the options/labels map to clean up the labels
-    console.log(data, breakdown1)
+    
     const getBreakdowns = (key) => key.split("__");
     
     const columns = Object.keys(data[0] || {})
         .filter((k) => k !== "period")
         .map(getBreakdowns);
 
-
+    
     let uniqueBreakdown1 = [...new Set(columns.map(([b1]) => b1))];
+    let uniqueBreakdown1Labels = [...new Set(columns.map(([b1]) => b1))];
     if(breakdown1 && breakdown1 != 'subcategory' && breakdown1 != 'indicator' && breakdown1 != 'Target'){
-        uniqueBreakdown1 = uniqueBreakdown1.map((val) => (map?.[breakdown1]?.[val] ?? val))
+        uniqueBreakdown1Labels = uniqueBreakdown1.map((val) => (map?.[breakdown1]?.[val] ?? val))
     }
     let uniqueBreakdown2 = [...new Set(columns.map(([, b2]) => b2).filter(Boolean))];
+    let uniqueBreakdown2Labels = [...new Set(columns.map(([, b2]) => b2).filter(Boolean))];
     if(breakdown2 && breakdown2 != 'subcategory'){
-        uniqueBreakdown2 = uniqueBreakdown2.map((val) => (map?.[breakdown2]?.[val] ?? val))
+        uniqueBreakdown2Labels = uniqueBreakdown2.map((val) => (map?.[breakdown2]?.[val] ?? val))
     }
 
     return (
@@ -30,17 +32,17 @@ export default function DataTable({ data, breakdown1, breakdown2, map }) {
                 <tr>
                 <th rowSpan={breakdown2 ? 2 : 1}>Period</th>
                 {breakdown2 ? (
-                    uniqueBreakdown1.map((b1) => (
+                    uniqueBreakdown1Labels.map((b1) => (
                     <th key={b1} colSpan={uniqueBreakdown2.length}>{b1}</th>
                     ))
                 ) : (
-                    uniqueBreakdown1.map((b1) => <th key={b1}>{cleanLabels(b1)}</th>)
+                    uniqueBreakdown1Labels.map((b1) => <th key={b1}>{cleanLabels(b1)}</th>)
                 )}
                 </tr>
                 {breakdown2 && (
                 <tr>
-                    {uniqueBreakdown1.map((b1) =>
-                    uniqueBreakdown2.map((b2) => (
+                    {uniqueBreakdown1Labels.map((b1) =>
+                    uniqueBreakdown2Labels.map((b2) => (
                         <th key={`${b1}__${b2}`}>{b2}</th>
                     ))
                     )}
