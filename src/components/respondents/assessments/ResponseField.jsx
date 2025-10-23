79ed01a5caeae52a88747f567936b5f2e6215728
Jay <rosato.jay@gmail.com>
2025-10-23 11:43:17 +0200

    Debugging

--- src/components/respondents/assessments/ResponseField.jsx
+++ src/components/respondents/assessments/ResponseField.jsx
@@ -60,7 +60,8 @@ export default function ResponseField ({ indicator, defaultVal, isVisible, onFie
             setValue(`response_data.${indicator.id}`, defaultVal, { shouldDirty: false });
         }
     }, [defaultVal, indicator.id, setValue]);
-
+    console.log(isVisible, defaultVal, value);
+    
     //field config object
     let fieldConfig = {
         type: convertType(indicator.type), 
